6f76861a4295a46e2ad4e7087592101f07aeceb3
vivcis <ceciliaorji.co@gmail.com>
2026-04-13 22:46:18 +0100

    create a cltv script

--- submission/04.sh
+++ submission/04.sh
@@ -1,2 +1,17 @@
 # Create a CLTV script with a timestamp of 1495584032 and public key below:
 # publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+
+#!/bin/bash
+
+publicKey="02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277"
+locktime=1495584032
+
+locktime_hex=$(printf '%08x' $locktime)
+locktime_le="${locktime_hex:6:2}${locktime_hex:4:2}${locktime_hex:2:2}${locktime_hex:0:2}"
+
+script="04${locktime_le}b17521${publicKey}ac"
+
+#verify with bitcoin-cli
+bitcoin-cli -regtest decodescript "$script"
+
+echo "$script"
